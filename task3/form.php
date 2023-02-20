81c139e7decb4b841a673cefb592aacdf54be4f7
IvanAngoldt <angoldt03@mail.ru>
2023-02-21 01:06:37 +0300

    html

--- task3/form.php
+++ task3/form.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+</head>
+<body>
 <form action="" method="POST">
   <input name="name" />
   <select name="year">
@@ -9,3 +18,5 @@
   </select>
   <input type="submit" value="ok" />
 </form>
+</body>
+</html>
