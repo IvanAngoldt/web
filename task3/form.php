224860eaa6039f9def4e6463d4cd5c824a6ba37c
IvanAngoldt <112856964+IvanAngoldt@users.noreply.github.com>
2023-02-20 18:41:59 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -1,4 +1,11 @@
 <form action="" method="POST">
   <input name="fio" />
+  <select name="year">
+    <?php 
+    for ($i = 1922; $i <= 2022; $i++) {
+      printf('<option value="%d">%d год</option>', $i, $i);
+    }
+    ?>
+  </select>
   <input type="submit" value="ok" />
 </form>
