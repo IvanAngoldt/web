1d447734352b790d3fc9daaa45e468e7325bd52f
IvanAngoldt <angoldt03@mail.ru>
2023-02-20 23:30:08 +0300

    back

--- task3/form.php
+++ task3/form.php
@@ -1,11 +1,4 @@
 <form action="" method="POST">
   <input name="fio" />
-  <select name="year">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>
   <input type="submit" value="ok" />
 </form>
