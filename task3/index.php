61dfacdedc9f1439d70e984d98d866a7a99009f6
IvanAngoldt <angoldt03@mail.ru>
2023-02-20 23:28:41 +0300

    back

--- task3/index.php
+++ task3/index.php
@@ -1,47 +1,64 @@
 <?php
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
   if (!empty($_GET['save'])) {
+    // Если есть параметр save, то выводим сообщение пользователю.
     print('Спасибо, результаты сохранены.');
   }
+  // Включаем содержимое файла form.php.
   include('form.php');
+  // Завершаем работу скрипта.
   exit();
 }
-
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+// Проверяем ошибки.
 $errors = FALSE;
 if (empty($_POST['fio'])) {
   print('Заполните имя.<br/>');
   $errors = TRUE;
 }
-
-if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
-  print('Заполните год.<br/>');
-  $errors = TRUE;
-}
-
+// *************
+// Тут необходимо проверить правильность заполнения всех остальных полей.
+// *************
 if ($errors) {
-  print('Попробуйте ещё раз.');
+  // При наличии ошибок завершаем работу скрипта.
   exit();
 }
-
+// Сохранение в базу данных.
 $user = 'u52855';
 $pass = '5599036';
-
+$db = new PDO('mysql:host=localhost;dbname=u52855', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+// Подготовленный запрос. Не именованные метки.
 try {
-  $db = new mysqli("localhost", "u52855", "5599036", "u52855");
-} catch (PDOException $e) {
-die($e->getMessage());
+  $stmt = $db->prepare("INSERT INTO application SET name = ?");
+  $stmt -> execute(array('fio'));
 }
-
-$fio = $_POST['fio'];
-
-$db->query("SET NAMES 'utf8'");
-$db->query("INSERT INTO form (fio) VALUES ($fio)");
-$db->close();
-if ($db->connect_error){
-    echo "Error Number: ".$db->connect_errno."<br>";
-    echo "Error: ".$db->connect_error;
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
 }
-
+//  stmt - это "дескриптор состояния".
+ 
+//  Именованные метки.
+//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
+//$stmt -> execute(array('label'=>'perfect', 'color'=>'green'));
+ 
+//Еще вариант
+/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+$stmt->bindParam(':firstname', $firstname);
+$stmt->bindParam(':lastname', $lastname);
+$stmt->bindParam(':email', $email);
+$firstname = "John";
+$lastname = "Smith";
+$email = "john@test.com";
+$stmt->execute();
+*/
+// Делаем перенаправление.
+// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
+// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
 header('Location: ?save=1');
-?>
