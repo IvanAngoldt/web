c5840971aaec9ca07c20d5ea58821fca0c1542d4
IvanAngoldt <angoldt03@mail.ru>
2023-02-21 01:14:33 +0300

    +css

--- task3/form.php
+++ task3/form.php
@@ -4,18 +4,23 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Form</title>
 </head>
 <body>
 <form action="" method="POST">
-  <input name="name" />
-  <select name="year">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>
+  <div class="form_item">
+    <input name="name" />
+  </div>
+  <div class="form_item">
+    <select name="year">
+      <?php 
+      for ($i = 1922; $i <= 2022; $i++) {
+        printf('<option value="%d">%d год</option>', $i, $i);
+      }
+      ?>
+    </select>
+  </div>
   <input type="submit" value="ok" />
 </form>
 </body>
