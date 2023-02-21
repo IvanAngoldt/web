fdff4f337db5d489e97d15737a95e5f0b40d189c
IvanAngoldt <angoldt03@mail.ru>
2023-02-21 20:18:48 +0300

    css

--- task3/index.php
+++ task3/index.php
@@ -4,7 +4,7 @@ header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_GET['save'])) {
-    print('Спасибо, результаты сохранены.');
+    print('<h1>Спасибо, результаты сохранены.</h1>');
   }
   include('form.php');
   exit();
