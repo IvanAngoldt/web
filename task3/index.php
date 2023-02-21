d1b72e02aca8f2448b172224134a8039e7e40c90
IvanAngoldt <angoldt03@mail.ru>
2023-02-21 19:34:06 +0300

    check

--- task3/index.php
+++ task3/index.php
@@ -2,13 +2,7 @@
 
 header('Content-Type: text/html; charset=UTF-8');
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  if (!empty($_GET['save'])) {
-    print('Спасибо, результаты сохранены.');
-  }
-  include('form.php');
-  exit();
-}
+
 
 $errors = FALSE;
 if (empty($_POST['name'])) {
