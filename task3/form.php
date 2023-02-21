39cce9aba3508d2abe7961e989ca53ce581b3f7a
IvanAngoldt <angoldt03@mail.ru>
2023-02-21 15:27:15 +0300

    css

--- task3/form.php
+++ task3/form.php
@@ -9,20 +9,29 @@
 </head>
 <body>
 <form class="form" action="" method="POST">
-  Имя: <input name="name" />
-  e-mail: <input name="email" />
-  ><label><input type="radio" checked="checked" name="sex" value="male"/>
+  <div class="form_item">
+    Имя: <input name="name" />
+  </div>
+  <div class="form_item">
+    e-mail: <input name="email" />
+  </div>
+  <div class="radio-button">
+  <label><input type="radio" checked="checked" name="sex" value="male"/>
     Мужчина
   </label>
   <label><input type="radio" name="sex" value="female" />
     Женщина
   </label>
-  Год: <select name="year"> <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-   ?>
-  </select>
+  </div>
+  <div class="form_item">
+    Год: <select name="year">
+      <?php 
+      for ($i = 1922; $i <= 2022; $i++) {
+        printf('<option value="%d">%d год</option>', $i, $i);
+      }
+      ?>
+    </select>
+  </div>
   <input class="input" type="submit" value="ok" />
 </form>
 </body>
