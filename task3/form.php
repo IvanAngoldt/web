241a30ceaaeb94fabdb06edbad093afcb414f49e
IvanAngoldt <angoldt03@mail.ru>
2023-02-21 16:55:20 +0300

    wqesda

--- task3/form.php
+++ task3/form.php
@@ -20,5 +20,14 @@
     <label>
       Женщина
     </label>
+
+  Конечностей:<input type="radio" name="limb" value="3" checked>
+    <label>
+      3
+    </label>
+  <input type="radio" name="limb" value="4">
+    <label>
+      4
+    </label>
   <input type="submit" value="ok" />
 </form>
