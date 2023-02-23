bd49c27c5d74598c335be487a4bf79ce24c9100f
IvanAngoldt <angoldt03@mail.ru>
2023-02-23 23:33:25 +0300

    truncate

--- showDB/index.php
+++ showDB/index.php
@@ -1,73 +1,39 @@
 <?php 
 
-include('db.php');
+    header('Content-Type: text/html; charset=UTF-8');
 
-$result1 = mysqli_query($induction, "SELECT * FROM `application`");
-$result2 = mysqli_query($induction, "SELECT * FROM `abilities`");
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if (!empty($_GET['deleted'])) {
+            print('
+                Таблицы очищены.
+            ');
+        }
+        include('truncate.php');
+    exit();
+    }
 
+    $host = "localhost";
+    $user = "u52855";
+    $pass = "5599036";
+    $name = "u52855";
+
+    $induction = mysqli_connect($host, $user, $pass, $name);
+
+    $result1 = mysqli_query($induction, "SELECT * FROM `application`");
+    $result2 = mysqli_query($induction, "SELECT * FROM `abilities`");
+
+    if (isset($_POST['myActionName'])) {
+        $query1 = "TRUNCATE TABLE application";
+        $query2 = "TRUNCATE TABLE abilities";
+        if ($induction == false) {
+            print "Ошибка подключения";
+        }
+        if (mysqli_multi_query($induction, $query1) && mysqli_multi_query($induction, $query2)) {
+            print "record deleted Successfully";
+        } else {
+            print "Error:" . mysqli_error($induction);
+        }
+    }
+
+    header('Location: ?deleted=1');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-  <link rel="stylesheet" href="style.css">
-  <title>Database u52855</title>
-</head>
-<body>
-  <table>
-    <caption>Данные из таблицы application</caption>
-    <tr> 
-      <th>&nbsp;</th>
-      <th>Имя</th>
-      <th>email</th>
-      <th>Год</th>
-      <th>Пол</th>
-      <th>Преобладающая рука</th>
-      <th>Биография</th>
-    </tr>
-    <?php
-    while ($form = mysqli_fetch_assoc($result1)) {
-      echo "
-        <tr>
-          <td>"; echo $form['id']; echo "</td>
-          <td>"; echo $form['name']; echo "</td>
-          <td>"; echo $form['email']; echo "</td>
-          <td>"; echo $form['year']; echo "</td>
-          <td>"; echo $form['sex']; echo "</td>
-          <td>"; echo $form['hand']; echo "</td>
-          <td>"; echo $form['biography']; echo "</td>
-        </tr>
-      ";
-    }
-    ?>
-  </table>
-  <table>
-    <caption>Данные из таблицы abilities</caption>
-    <tr> 
-      <th>&nbsp;</th>
-      <th>Бессмертие</th>
-      <th>Прохождение сквозь стены</th>
-      <th>Левитация</th>
-    </tr>
-    <?php
-    while ($form = mysqli_fetch_assoc($result2)) {
-      echo "
-        <tr>
-          <td>"; echo $form['id']; echo "</td>
-          <td>"; echo $form['god']; echo "</td>
-          <td>"; echo $form['noclip']; echo "</td>
-          <td>"; echo $form['levitation']; echo "</td>
-        </tr>
-      ";
-    }
-    ?>
-  </table>
-  <form action="truncate.php" method="POST">
-     <input name="myActionName" type="submit" value="Очистить данные" />
-  </form>
-</body>
-</html>
--- showDB/truncate.php
+++ showDB/truncate.php
@@ -1,19 +1,76 @@
-<?php 
-    if (isset($_POST['myActionName'])) {
-        $host = "localhost";
-        $user = "u52855";
-        $pass = "5599036";
-        $name = "u52855";
-        $connection = mysqli_connect($host, $user, $pass, $name);
-        $query1 = "TRUNCATE TABLE application";
-        $query2 = "TRUNCATE TABLE abilities";
-        if ($connection == false) {
-            print "Ошибка подключения";
-        }
-        if (mysqli_multi_query($connection, $query1) && mysqli_multi_query($connection, $query2)) {
-            print "record deleted Successfully";
-        } else {
-            print "Error:" . mysqli_error($connection);
-        }
-    }
+<?php
+  $host = "localhost";
+  $user = "u52855";
+  $pass = "5599036";
+  $name = "u52855";
+
+  $induction = mysqli_connect($host, $user, $pass, $name);
+
+  $result1 = mysqli_query($induction, "SELECT * FROM `application`");
+  $result2 = mysqli_query($induction, "SELECT * FROM `abilities`");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+  <link rel="stylesheet" href="style.css">
+  <title>Database u52855</title>
+</head>
+<body>
+  <table>
+    <caption>Данные из таблицы application</caption>
+    <tr> 
+      <th>&nbsp;</th>
+      <th>Имя</th>
+      <th>email</th>
+      <th>Год</th>
+      <th>Пол</th>
+      <th>Преобладающая рука</th>
+      <th>Биография</th>
+    </tr>
+    <?php
+    while ($form = mysqli_fetch_assoc($result1)) {
+      echo "
+        <tr>
+          <td>"; echo $form['id']; echo "</td>
+          <td>"; echo $form['name']; echo "</td>
+          <td>"; echo $form['email']; echo "</td>
+          <td>"; echo $form['year']; echo "</td>
+          <td>"; echo $form['sex']; echo "</td>
+          <td>"; echo $form['hand']; echo "</td>
+          <td>"; echo $form['biography']; echo "</td>
+        </tr>
+      ";
+    }
+  ?>
+  </table>
+  <table>
+    <caption>Данные из таблицы abilities</caption>
+    <tr> 
+      <th>&nbsp;</th>
+      <th>Бессмертие</th>
+      <th>Прохождение сквозь стены</th>
+      <th>Левитация</th>
+    </tr>
+    <?php
+    while ($form = mysqli_fetch_assoc($result2)) {
+      echo "
+        <tr>
+          <td>"; echo $form['id']; echo "</td>
+          <td>"; echo $form['god']; echo "</td>
+          <td>"; echo $form['noclip']; echo "</td>
+          <td>"; echo $form['levitation']; echo "</td>
+        </tr>
+      ";
+    }
+    ?>
+  </table>
+  <form action="" method="POST">
+     <input name="myActionName" type="submit" value="Очистить данные" />
+  </form>
+</body>
+</html>
