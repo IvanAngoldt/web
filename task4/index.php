008946e83457a9abff79fcc1ff7709fb1e2067bf
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-03-26 21:26:41 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -214,7 +214,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     print('Error : ' . $e->getMessage());
     exit();
   }
-  $test = 'test';
   setcookie('save', '1');
   header('Location: ?save=1');
 }
