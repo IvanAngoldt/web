4cfa2648addb1a979e36afa81674dd6a46580d83
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-03-27 14:06:58 +0300

    Update form.php

--- task4/form.php
+++ task4/form.php
@@ -20,18 +20,14 @@
     </div>
     <div class="form-content">
       <div class="form-item">
-        <div class="group1">  
-          <input class="line" name="name" placeholder="" value="<?php echo $values['name']; ?>" />
-          <label class="labelText <?php if ($errors['name']) {print 'error';} ?>">Имя</label>
-          <?php if ($errors['name']) {print $messages['name'];} ?>
-        </div>
+        <p <?php if ($errors['name']) {print 'class="error"';} ?>>Имя</p>
+        <input class="line" name="name" value="<?php echo $values['name']; ?>" />
+        <?php if ($errors['name']) {print $messages['name'];} ?>
       </div>
       <div class="form-item">
-        <div class="group2">
-          <input class="line" name="email" placeholder="" value="<?php print $values['email']; ?>" />
-          <label class="labelText <?php if ($errors['email1'] || $errors['email2']) {print 'error';} ?>">Email</label>
-          <?php if ($errors['email1']) {print $messages['email1'];} else if ($errors['email2']) {print $messages['email2'];}?>
-        </div>
+        <p <?php if ($errors['email1'] || $errors['email2']) {print 'class="error"';} ?>>Email</p>
+        <input class="line" name="email" value="<?php print $values['email']; ?>" />
+        <?php if ($errors['email1']) {print $messages['email1'];} else if ($errors['email2']) {print $messages['email2'];}?>
       </div>
       <div class="form-item">
         <div class="date">
