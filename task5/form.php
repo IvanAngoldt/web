a2895fd303bf49e87b1f57cd23f02cfcef3ed076
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-04-03 14:25:29 +0300

    Add files via upload

---
+++ task5/form.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <style>
+/* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
+.error {
+  border: 2px solid red;
+}
+    </style>
+  </head>
+  <body>
+
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
+?>
+
+    <form action="" method="POST">
+      <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
+      <input type="submit" value="ok" />
+    </form>
+  </body>
+</html>
