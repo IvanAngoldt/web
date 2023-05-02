e342714292b7ad84652fd54ffb02359810bed9b6
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-05-03 02:35:23 +0300

    Delete dbshow directory

--- dbshow/dbconnect.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$user = 'u52855';
-$pass = '5599036';
-$db = new PDO('mysql:host=localhost;dbname=u52855', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
--- dbshow/truncate.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('dbconnect.php');
-$stmt = $db->prepare("TRUNCATE application; TRUNCATE abilities; TRUNCATE users");
-$stmt->execute();
-header('Location: index.php');
