b1f1ab7a2f35b8fc87ff81f9c647fdf5ba8f6fea
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-05-03 02:35:16 +0300

    Delete task6 directory

--- task6/admin.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE admins (
-  admin_id int(128) unsigned NOT NULL AUTO_INCREMENT,
-  login varchar(16) NOT NULL DEFAULT '',
-  password varchar(32) NOT NULL DEFAULT '',
-  PRIMARY KEY (admin_id)
-);
