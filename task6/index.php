03c27c3d06dfb4ade1ddc4435662d5a82e2b6f5a
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-05-20 21:07:04 +0300

    Update index.php

--- task6/index.php
+++ task6/index.php
@@ -15,50 +15,60 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         setcookie('edit', '', time() + 24 * 60 * 60);
     }
     include('errors.php');
+
+    
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+    $_SESSION['login'] = $validUser;
+
     include('dbshow.php');
     exit();
 } else {
-    foreach ($_POST as $key => $value) {
-        if (preg_match('/^clear(\d+)_x$/', $key, $matches)) {
-            $app_id = $matches[1];
-            setcookie('clear', $app_id, time() + 24 * 60 * 60);
-            $stmt = $db->prepare("DELETE FROM application WHERE application_id = ?");
-            $stmt->execute([$app_id]);
-            $stmt = $db->prepare("DELETE FROM abilities WHERE application_id = ?");
-            $stmt->execute([$app_id]);
-            $stmt = $db->prepare("DELETE FROM users WHERE application_id = ?");
-            $stmt->execute([$app_id]);
-        }
-        if (preg_match('/^edit(\d+)_x$/', $key, $matches)) {
-            $app_id = $matches[1];
-            setcookie('edit', $app_id, time() + 24 * 60 * 60);
-        }
-        if (preg_match('/^save(\d+)_x$/', $key, $matches)) {
-            setcookie('edit', '', time() + 24 * 60 * 60);
-            $app_id = $matches[1];
-            include('errors.php');
-            $stmt = $db->prepare("SELECT name, email, year, sex, hand, biography FROM application WHERE application_id = ?");
-            $stmt->execute([$app_id]);
-            $old_dates = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            $stmt = $db->prepare("SELECT superpower_id FROM abilities WHERE application_id = ?");
-            $stmt->execute([$app_id]);
-            $old_abilities = $stmt->fetchAll(PDO::FETCH_COLUMN);
-            if (array_diff($dates, $old_dates[0])) {
-                setcookie('updated', $app_id, time() + 24 * 60 * 60);
-                $stmt = $db->prepare("UPDATE application SET name = ?, email = ?, year = ?, sex = ?, hand = ?, biography = ? WHERE application_id = ?");
-                $stmt->execute([$dates['name'], $dates['email'], $dates['year'], $dates['sex'], $dates['hand'], $dates['biography'], $app_id]);
-            }
-            if (array_diff($abilities, $old_abilities) || count($abilities) != count($old_abilities)) {
-                setcookie('updated', $app_id, time() + 24 * 60 * 60);
+    if (!empty($_POST['token']) && hash_equals($_POST['token'], $_SESSION['token'])) {
+        foreach ($_POST as $key => $value) {
+            if (preg_match('/^clear(\d+)_x$/', $key, $matches)) {
+                $app_id = $matches[1];
+                setcookie('clear', $app_id, time() + 24 * 60 * 60);
+                $stmt = $db->prepare("DELETE FROM application WHERE application_id = ?");
+                $stmt->execute([$app_id]);
                 $stmt = $db->prepare("DELETE FROM abilities WHERE application_id = ?");
                 $stmt->execute([$app_id]);
-                $stmt = $db->prepare("INSERT INTO abilities (application_id, superpower_id) VALUES (?, ?)");
-                foreach ($abilities as $superpower_id) {
-                    $stmt->execute([$app_id, $superpower_id]);
+                $stmt = $db->prepare("DELETE FROM users WHERE application_id = ?");
+                $stmt->execute([$app_id]);
+            }
+            if (preg_match('/^edit(\d+)_x$/', $key, $matches)) {
+                $app_id = $matches[1];
+                setcookie('edit', $app_id, time() + 24 * 60 * 60);
+            }
+            if (preg_match('/^save(\d+)_x$/', $key, $matches)) {
+                setcookie('edit', '', time() + 24 * 60 * 60);
+                $app_id = $matches[1];
+                include('errors.php');
+                $stmt = $db->prepare("SELECT name, email, year, sex, hand, biography FROM application WHERE application_id = ?");
+                $stmt->execute([$app_id]);
+                $old_dates = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                $stmt = $db->prepare("SELECT superpower_id FROM abilities WHERE application_id = ?");
+                $stmt->execute([$app_id]);
+                $old_abilities = $stmt->fetchAll(PDO::FETCH_COLUMN);
+                if (array_diff($dates, $old_dates[0])) {
+                    setcookie('updated', $app_id, time() + 24 * 60 * 60);
+                    $stmt = $db->prepare("UPDATE application SET name = ?, email = ?, year = ?, sex = ?, hand = ?, biography = ? WHERE application_id = ?");
+                    $stmt->execute([$dates['name'], $dates['email'], $dates['year'], $dates['sex'], $dates['hand'], $dates['biography'], $app_id]);
+                }
+                if (array_diff($abilities, $old_abilities) || count($abilities) != count($old_abilities)) {
+                    setcookie('updated', $app_id, time() + 24 * 60 * 60);
+                    $stmt = $db->prepare("DELETE FROM abilities WHERE application_id = ?");
+                    $stmt->execute([$app_id]);
+                    $stmt = $db->prepare("INSERT INTO abilities (application_id, superpower_id) VALUES (?, ?)");
+                    foreach ($abilities as $superpower_id) {
+                        $stmt->execute([$app_id, $superpower_id]);
+                    }
                 }
             }
         }
+        
+    } else {
+        die('Ошибка CSRF: недопустимый токен');
     }
     header('Location: index.php');
 }
