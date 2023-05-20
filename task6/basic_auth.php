f67dfa1ade87b13ca0d5d9b452069d7b8418147a
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-05-20 21:06:31 +0300

    Update basic_auth.php

--- task6/basic_auth.php
+++ task6/basic_auth.php
@@ -19,10 +19,11 @@ if ($row) {
 if ($_SERVER['PHP_AUTH_USER'] != $validUser || md5($_SERVER['PHP_AUTH_PW']) != $validPassHash) {
     requireLogin();
 }
+session_start();
 
 function requireLogin() {
     header('HTTP/1.1 401 Unanthorized');
-    header('WWW-Authenticate: Basic realm="My site"');
+    header('WWW-Authenticate: Basic realm="Task6"');
     print('<h1>401 Требуется авторизация</h1>');
     exit();
 }
